fb223436ae0e02ef1357a0588b9410b565dd3a2b
sodi <sd@yodiwo.com>
2017-11-27 10:44:57 +0200

    Add Nancy.Serialization.JsonNet in the repository (3c0f042f54755fb3610b71e97ec5c51d58d8ea87)

---
+++ src/Nancy.Serialization.JsonNet/Helper.cs
@@ -0,0 +1,31 @@
+﻿namespace Nancy.Serialization.JsonNet
+{
+    using Nancy.Responses.Negotiation;
+    using System;
+
+    internal static class Helpers
+    {
+        /// <summary>
+        /// Attempts to detect if the content type is JSON.
+        /// Supports:
+        ///   application/json
+        ///   text/json
+        ///   application/vnd[something]+json
+        /// Matches are case insentitive to try and be as "accepting" as possible.
+        /// </summary>
+        /// <param name="contentType">Request content type</param>
+        /// <returns>True if content type is JSON, false otherwise</returns>
+
+
+        public static bool IsJsonType(MediaRange contentType)
+        {
+            if (string.IsNullOrEmpty(contentType))
+            {
+                return false;
+            }
+
+            return contentType.Matches("application/json") || contentType.Matches("text/json") || contentType.Matches("application/vnd") || contentType.Matches("+json");
+
+        }
+    }
+}
